2d34ad0396c806c322b89bb419f2120f8b96215b
andreev5r <andreev.petar.biz@gmail.com>
2024-02-23 15:03:36 +0200

    init

---
+++ src/main/java/com/apex/tech3/wallt_app/WalltAppApplication.java
@@ -0,0 +1,13 @@
+package com.apex.tech3.wallt_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalltAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalltAppApplication.class, args);
+    }
+
+}
