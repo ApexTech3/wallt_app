5c566a9a29c7db66a4e95d8573b05ff710cc4b69
Boyan Buliev <boyanbot@gmail.com>
2024-02-26 20:52:29 +0200

    added swagger implementation

--- src/main/java/com/apex/tech3/wallt_app/WalltAppApplication.java
+++ src/main/java/com/apex/tech3/wallt_app/WalltAppApplication.java
@@ -1,9 +1,13 @@
 package com.apex.tech3.wallt_app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Forum Application API Documentation", version = "10.0",
+        description = "This is the documentation for our team project"))
 public class WalltAppApplication {
 
     public static void main(String[] args) {
